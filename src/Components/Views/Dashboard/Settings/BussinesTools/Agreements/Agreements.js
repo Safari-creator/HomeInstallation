218ad11db62cc7801be553aca8e795384c0cf669
Safari-creator <evansmusafa@gmail.com>
2021-12-14 19:37:49 +0530

    style

--- src/Components/Views/Dashboard/Settings/BussinesTools/Agreements/Agreements.js
+++ src/Components/Views/Dashboard/Settings/BussinesTools/Agreements/Agreements.js
@@ -3,7 +3,23 @@ import React from 'react'
 function Agreement() {
     return (
         <div>
-            I am agreement
+            <div className="company-settings-Calendar-row">
+                <div className="left-col">
+                    Inspect Agreement
+                </div>
+                {/* <div className="right-col">
+                    <p>Allows you to preview customizable info about an inspection when hovering over it on the calendar. Note that this may slow down calendar load times due to more data being loaded!</p>
+                </div> */}
+            </div>
+            <div className="company-settings-Calendar-row">
+                <div className="left-col">
+
+                </div>
+                <div className="right-col">
+                    {/* <p>Customize what you see when hovering over an inspection or event on the calendar. Note that this setting applies to everyone in the company.</p> */}
+                    <div className="customize-hover-btn">CREATE A NEW INSPECTION AGREEMENT</div>
+                </div>
+            </div>
         </div>
     )
 }
